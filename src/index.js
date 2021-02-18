a28edd0474870d58ed52166b293a4fbbe869d883
handsomeaddress <yeiehahaha@gmail.com>
2021-02-18 20:34:55 +0300

    init: Brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	bracketsConfig = bracketsConfig.map((x)=> x.join(''));
+	for (let i = 0; i < bracketsConfig.length;){
+		if(str.includes(bracketsConfig[i])){
+			str = str.split(bracketsConfig[i]).join('');
+			i = 0;
+			continue;
+		}
+		i++;
+	}
+	return str ? false : true;
 }
